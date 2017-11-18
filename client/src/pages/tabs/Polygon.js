a488e6d1abebe2449082a0d0f5fdd1c4f2dd769f
Elizabeth Prieto <eprieto.business@gmail.com>
2017-11-18 11:03:00 -0500

    tabs within the news page

---
+++ client/src/pages/tabs/Polygon.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Polygon = () =>
+  <div className="polygon-headlines">
+    <div>
+		<h1> Polygon Headlines</h1>
+			<h3>First News Article</h3>
+			<img src="https://lh4.ggpht.com/YTDiP6yryhXLOFaG7-ZY0kkpRgAUQ-dipJytepR-pmiJO_RJqemDDiNhS_LvUCkoThA=w120" alt="pic5"/>
+ 			<p> Paragraph with summaries of articles. </p>
+		</div>
+		<div>
+			<h3>Second News Article</h3>
+      <img src="http://classwithapps.com/wp-content/uploads/2013/02/settings.png" alt="pic4"/>
+			<p> Paragraph with summaries of articles. </p>
+		</div>
+		<div>
+			<h3>Third News Article</h3>
+			<img src="https://cdn0.iconfinder.com/data/icons/social-glyph/30/vkontakte-120.png" alt="pic2"/>
+      <p> Paragraph with summaries of articles. </p>
+		</div>
+		<div>
+			<h3>Fourth News Article</h3>
+      <img src="http://www.iconninja.com/files/503/812/914/meetup-icon.png" alt="pic3"/>
+			<p> Paragraph with summaries of articles. </p>
+		</div>
+		<div>
+			<h3>Fifth News Article</h3>
+      <img src="https://cdn0.iconfinder.com/data/icons/social-glyph/30/twitch-120.png" alt="pic1"/>
+			<p> Paragraph with summaries of articles. </p>
+		</div>
+  </div>
+
+export default Polygon; 
