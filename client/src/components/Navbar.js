7db73723bb296b6ea5c0649d2f2c0d5f116c4e52
mnelson11 <mackenziernelson@gmail.com>
2017-11-21 20:19:42 -0500

    added icon images

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import logo from '../images/game-rally7.png';
-import game from '../images/23385-200.png';
-import gameboy from '../images/video-game-1856991_960_720.png';
-import controller from '../images/icon-game-controller-a-512.png';
+
 
 
 const navbarStyle = {
@@ -18,6 +16,7 @@ const navbarStyle = {
 const iconStyle = {
   width: "125px"
 }
+
 const containerStyle ={
   background: "-webkit-gradient(linear, center top, center bottom, from(#fff), to(#ccc))",
   backgroundImage: "linear-gradient(#fff, #ccc)",
