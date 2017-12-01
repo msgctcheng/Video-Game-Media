146ae4f79284bec80c1f09d5c10f1a7400b06510
mnelson11 <mackenziernelson@gmail.com>
2017-11-30 20:51:09 -0500

    additional style changes to article search

--- client/src/components/ArticlesList/ArticlesItem.js
+++ client/src/components/ArticlesList/ArticlesItem.js
@@ -5,27 +5,42 @@ const article = {
   padding:"10px",
   textAlign:"center",
   backgroundImage: "linear-gradient(black, #0092CA)",
-  color:"white",
-  fontFamily: "'Lora', serif",
+
+}
+
+const caption = {
+  color:"black",
+  fontSize:"17px"
+}
+
+const captionTitle = {
+    fontFamily: "'Lora', serif",
+    color:"silver",
 
 }
 
 const buttons = {
- margin:"10px",
-  backgroundImage: "linear-gradient(black, #0092CA)",
-  color:"white",
+  margin:"10px",
+  color:"black",
+  fontFamily: "'Lora', serif",
+  background: "-webkit-gradient(linear, center top, center bottom, from(#fff), to(#ccc))",
+  backgroundImage: "linear-gradient(#fff, #ccc)",
+  borderRadius: "9px",
+  boxShadow: "0px 0px 4px 2px rgba(0,0,0,0.4)",
+  height: "35px",
+
 
 }
 // renders a bootstrap list item containing data from the api call
 export const ArticlesItem = props => (
   <div  className = "row" >
 
-    <div style = {article} className = "col-md-4">
-      <div  className="thumbnail">
+    <div  className = "col-md-4">
+      <div style = {article} className="thumbnail">
         <img src={props.img} alt="img"/>
-      <div className="caption">
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
+      <div>
+        <h3 style = {captionTitle}>{props.title}</h3>
+        <p style = {caption}>{props.description}</p>
         <p><a style = {buttons} className="btn btn-primary" role="button" href={props.url}>Go to Article!</a>
         <a style = {buttons} className="btn btn-primary" role="button" href={props.url}>Save!</a></p>
       </div>
