ba2b300ed574ebaabd3074d1d03946a1a1693c14
mnelson11 <mackenziernelson@gmail.com>
2017-12-06 15:38:13 -0500

    changed main js and website item js to make the buttons appear within the card and change button style

--- client/src/components/Websites/WebsiteItem.js
+++ client/src/components/Websites/WebsiteItem.js
@@ -1,8 +1,18 @@
 import React from "react";
 
+const buttons = {
+  margin:"10px",
+  color:"black",
+  fontFamily: "'Lora', serif",
+  background: "-webkit-gradient(linear, center top, center bottom, from(#fff), to(#ccc))",
+  backgroundImage: "linear-gradient(#fff, #ccc)",
+  borderRadius: "9px",
+  boxShadow: "0px 0px 4px 2px rgba(0,0,0,0.4)",
+  height: "35px",
+}
 export const WebsiteItem = props => (
-<div>
+<div className = "col-md-4">
     
-    <p><a className="btn btn-primary" role="button" href={props.url}>Go to Site</a></p>
-    </div>
+    <p><a className="btn btn-primary" style={buttons} role="button" href={props.url}>Go to Site</a></p>
+</div>
 );
