0b9d750fc0adb5c4c060c7eaadc304502db690e1
msgctcheng <msgctcheng@gmail.com>
2017-12-05 21:44:05 -0500

    Spoof the login

---
+++ client/src/Login.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
+
+class Login extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+    return (
+      <div id="loginBox">
+            {
+              !isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
+                )
+            }
+        
+      </div>
+    );
+  }
+}
+
+export default Login;
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
@@ -22,31 +22,6 @@ const userSchema = new Schema ({
     }
 });
 
-// userSchema.pre('save', function(next) {  
-//     const user = this,
-//           SALT_FACTOR = 5;
-  
-//     if (!user.isModified('password')) return next();
-  
-//     bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
-//       if (err) return next(err);
-  
-//       bcrypt.hash(user.password, salt, null, function(err, hash) {
-//         if (err) return next(err);
-//         user.password = hash;
-//         next();
-//       });
-//     });
-//   });
-  
-  // Method to compare password for login
-  userSchema.methods.comparePassword = function(candidatePassword, cb) {  
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-      if (err) { return cb(err); }
-  
-      cb(null, isMatch);
-    });
-  }
 
  const User = mongoose.model("User", userSchema);
 
